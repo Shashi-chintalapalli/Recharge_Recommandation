d63102ff08046861150423d8edb19d87df23bf1c
shashi chintalapalli <85443954+Shashi-chintalapalli@users.noreply.github.com>
2025-06-29 19:52:29 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,19 +2,21 @@ from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
 import joblib
+import os
 from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 
-# Load model and tools
-model = load_model("/Users/shashichintalapalli/Desktop/Recharge_Recommandation/models/dl_vi_recharge_model.keras")
-scaler = joblib.load("/Users/shashichintalapalli/Desktop/Recharge_Recommandation/models/scaler.pkl")
-label_encoders = joblib.load("/Users/shashichintalapalli/Desktop/Recharge_Recommandation/models/label_encoders.pkl")
+# Dynamically resolve paths
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+model = load_model(os.path.join(BASE_DIR, "../models/dl_vi_recharge_model.keras"))
+scaler = joblib.load(os.path.join(BASE_DIR, "../models/scaler.pkl"))
+label_encoders = joblib.load(os.path.join(BASE_DIR, "../models/label_encoders.pkl"))
 
 @app.route("/", methods=["GET", "POST"])
 def predict():
     if request.method == "POST":
-        # Collect form inputs
         user_input = {
             'data_usage_GB': float(request.form['data_usage_GB']),
             'call_minutes': int(request.form['call_minutes']),
@@ -25,7 +27,6 @@ def predict():
             'user_type': request.form['user_type']
         }
 
-        # Create DataFrame
         df = pd.DataFrame([user_input])
 
         # Encode categorical features
@@ -37,15 +38,13 @@ def predict():
         df['calls_per_recharge'] = df['call_minutes'] / df['avg_recharge']
         df['sms_per_recharge'] = df['sms_count'] / df['avg_recharge']
 
-        # Scale
         X_scaled = scaler.transform(df)
 
-        # Predict
         prediction = model.predict(X_scaled)
         predicted_class = int(np.argmax(prediction))
         predicted_label = label_encoders['recommended_plan'].inverse_transform([predicted_class])[0]
-        return render_template("index.html", prediction=predicted_label)
 
+        return render_template("index.html", prediction=predicted_label)
 
     return render_template("index.html", prediction=None)
 
